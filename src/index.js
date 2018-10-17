d3f69bfc47a70544c1ae84c0a3d60b71bc8372eb
dnwu <443881762@qq.com>
2018-10-17 10:22:28 +0800

    添加redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import IRouter from './router'
+import { Provider } from 'react-redux'
+import configStore from './redux/store'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<IRouter />, document.getElementById('root'));
+const store = configStore()
+
+ReactDOM.render(
+    <Provider store={store}>
+        <IRouter />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
